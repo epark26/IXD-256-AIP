a432d8855266404c0e9055eb3183ab98a5407826
epark26 <epark26@inside.artcenter.edu>
2023-10-13 11:17:43 -0700

    Book.mark_py.py

---
+++ Assignment3/book.mark_py.py
@@ -0,0 +1,40 @@
+
+import os, sys, io
+import M5
+from M5 import *
+import time
+from unit import *
+
+
+angle_0 = None
+
+
+angle_value = None
+
+
+def setup():
+  global angle_0, angle_value
+
+  angle_0 = Angle((1,2))
+  M5.begin()
+
+
+def loop():
+  global angle_0, angle_value
+  print(angle_value)
+  M5.update()
+  angle_value = angle_0.get_value()
+  time.sleep_ms(500)
+
+
+if __name__ == '__main__':
+  try:
+    setup()
+    while True:
+      loop()
+  except (Exception, KeyboardInterrupt) as e:
+    try:
+      from utility import print_error_msg
+      print_error_msg(e)
+    except ImportError:
+      print("please update to latest firmware")
