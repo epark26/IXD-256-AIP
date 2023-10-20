a7c786b1c82ce0b5c86ffd350d57ace527f8c3f5
epark26 <146476062+epark26@users.noreply.github.com>
2023-10-20 11:03:32 -0700

    Update main.py

--- TestAssignment/main.py
+++ TestAssignment/main.py
@@ -16,7 +16,20 @@ def draw():
   circle_size = int(data)
   p5.noStroke()
   p5.fill(150)
-  p5.ellipse(150, 150, circle_size, circle_size)
+  #p5.ellipse(150, 150, circle_size, circle_size)
+  p5.push()
+  #set angle variable to integer of data
+  angle = int(data)
+  p5.translate(p5.width/2, p5.height/2)
+  #rotate canvas with angle converted from degrees to radians:
+  p5.rotate(p5.radians(angle))
+  #change mode to draw rectangles from center:
+  p5.rectMode(p5.center)
+  #draw rectangle at coordinate 0, 0 and 100 width and height:
+  p5.rect(0, 0, 100, 100)
+  #rectore graphical transformation:
+  p5.pop()
+  
 
 def print_test(x):
   print(x)
